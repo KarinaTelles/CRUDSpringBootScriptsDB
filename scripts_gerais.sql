db91f315ac2bf6c0796111050436e90bff9326bd
Karina Telles <karinac.telles@gmail.com>
2025-07-30 20:32:20 -0300

    Update scripts_gerais.sql

--- scripts_gerais.sql
+++ scripts_gerais.sql
@@ -1,8 +1,9 @@
 -- comandos perigosos! use com cuidado, rs
+drop schema vollmed_api;
+drop schema med_api;
 create database med_api;
-select * from med_api.flyway_schema_history;
-drop table med_api.medicos; 
 delete from med_api.flyway_schema_history where version = '1';
+select * from med_api.flyway_schema_history;
 -- selecionando a tabela
 select * from med_api.medicos;
 desc med_api.medicos;
